03acf1b477b0d5d1bc64f5d0bc01e9b315eece85
ToonasBoi <joonas.vuorio@icloud.com>
2024-08-05 21:05:20 +0300

    idk
    idk

--- test.go
+++ test.go
@@ -1,7 +1,33 @@
 package pxs_engine
 
-import "fmt"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
-func HelloWorld() {
-	fmt.Println("Hello, world!")
+var config struct {
+	win_w int32
+	win_h int32
+	res_w int32
+	res_h int32
+
+	title string
+}
+
+func Config(win_w int32, win_h int32, res_w int32, res_h int32, title string) {
+	config.win_w = win_w
+	config.win_h = win_h
+	config.res_w = res_w
+	config.res_h = res_h
+	config.title = title
+}
+
+func Launch(start_func func(), update_func func(float32), draw_func func()) {
+	rl.InitWindow(config.win_w, config.win_h, config.title)
+
+	start_func()
+
+	for !rl.WindowShouldClose() {
+		update_func(float32(rl.GetTime()))
+		draw_func()
+	}
 }
